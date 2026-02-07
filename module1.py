f2038d96e1ea7816a06d4e95acb97bee33b22e8a
chiraggk5 <chiraggk8@gmail.com>
2026-02-07 20:34:41 +0530

    Add files via upload
    Module 1

---
+++ module1.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import re 
+import string
+
+STOPWORDS = {
+    "is","the","and","a","an","to","of","in","on","for","with","this","that","it","was","are","as","at","be","by","from","or","but"
+}
+
+def clean_text(text):
+    text = str(text).lower()
+    text = re.sub(r"http\S+","",text) 
+    text = re.sub(r"\d+","",text) 
+    text = text.translate(str.maketrans("","",string.punctuation)) 
+    text = re.sub(r"\s+","",text).strip() 
+    words = [W for W in text.split() if W not in STOPWORDS] 
+    return " ".join(words)
+
+def main():
+    file_path_excel = "ReviewSense_Customer_Feedback_5000.xlsx"
+    file_path_csv = "Module1_Cleaned_Feedback.csv"
+
+    try:
+        df = pd.read_excel(file_path_excel) 
+        print("Reading from Excel file")
+    except FileNotFoundError:
+        print("Excel file not found. Attempting to read from existing CSV.")
+        try:
+            df = pd.readcsv(file_path_csv)
+            if "clean_feedback" in df.columns:
+                print("CSV already has cleaned feedback. Skipping cleaning.")
+                return
+            elif "feedback" in df.columns:
+                print("Re-Cleaning feedback from CSV.")
+            else:
+                raise ValueError("'Feedback' column not found in CSV file.")
+        except FileNotFoundError:
+            raise ValueError("Neither Excel nor CSV file found. Please provide the Excel file to proceed.")
+    if "feedback" not in df.columns:
+        raise ValueError("'Feedback' column not found in the data. Please check the file format.")
+    
+    df["clean_feedback"] = df["feedback"].apply(clean_text)
+    df.to_csv("Module1_Cleaned_Feedback.csv",index=False)
+    print("Module 1 completed successfully.")
+    print(df[["feedback","clean_feedback"]].head())
+    
+if __name__ == "__main__":
+    main()
